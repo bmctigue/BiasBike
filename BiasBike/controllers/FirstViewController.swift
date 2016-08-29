76de9fbc29a3d0482b376b87f80f1e986634c699
Bruce McTigue <bmctigue@tiguer.com>
2016-08-29 16:53:14 -0600

    Initial Commit

---
+++ BiasBike/controllers/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BiasBike
+//
+//  Created by Bruce McTigue on 8/29/16.
+//  Copyright © 2016 tiguer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
