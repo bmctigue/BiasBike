cfe252cbedc842ccb89340579af0476d9cb30e72
Bruce McTigue <bmctigue@tiguer.com>
2016-08-30 09:52:31 -0600

    Added the Category enum

---
+++ BiasBike/models/Category.swift
@@ -0,0 +1,19 @@
+//
+//  Category.swift
+//  BiasBike
+//
+//  Created by Bruce McTigue on 8/30/16.
+//  Copyright © 2016 tiguer. All rights reserved.
+//
+
+import Foundation
+
+enum Category: String {
+    case World
+    case News
+    case Myths
+    case Religion
+    case Health
+    case Science
+    case Politics
+}
