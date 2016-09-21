03611d140f6c0e302ef123cef3620c3798d3617f
Bruce McTigue <bmctigue@tiguer.com>
2016-09-21 07:28:58 -0600

    Added enum for rating images

--- BiasBike/cells/CellUtilities.swift
+++ BiasBike/cells/CellUtilities.swift
@@ -8,18 +8,25 @@
 
 import Foundation
 
+enum RatingImage: String {
+    case Green = "green_cell_blank"
+    case Yellow = "yellow_cell_blank"
+    case Orange = "orange_cell_blank"
+    case Red = "red_cell_blank"
+}
+
 struct CellUtitilities {
     
     func ratingImageNameString(rating: Int) -> String {
         var ratingImageName: String
         if rating <= 25 {
-            ratingImageName = "red_cell_blank"
+            ratingImageName = RatingImage.Red.rawValue
         } else if rating <= 50 {
-            ratingImageName = "orange_cell_blank"
+            ratingImageName = RatingImage.Orange.rawValue
         } else if rating <= 75 {
-            ratingImageName = "yellow_cell_blank"
+            ratingImageName = RatingImage.Yellow.rawValue
         } else {
-            ratingImageName = "green_cell_blank"
+            ratingImageName = RatingImage.Green.rawValue
         }
         return ratingImageName
     }
